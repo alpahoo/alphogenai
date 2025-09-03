3701807b3ef5399439449ba39c476edb1abaf409
alpahoo <digitalpaho@outlook.com>
2025-09-03 03:18:06 +0200

    Create index.js

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 32}}>
+      <h1>AlphoGenAI</h1>
+      <p>Starter app deployed on Cloudflare Pages.</p>
+    </main>
+  )
+}
